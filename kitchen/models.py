f64913bb35868dc6829b78b10f41a6004cfa3a14
Yurii <yura.gvozd.2002@gmail.com>
2026-08-21 17:20:01 +0300

    add Dish, DishType, Ingredient, Cook models & configure AUTH_USER_MODEL and installed apps in settings

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username})"
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='dishes')
+    ingredients = models.ManyToManyField(Ingredient, blank=True, related_name='dishes')
+
+    def __str__(self):
+        return f"{self.name} ({self.price}$)"
+
--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant_kitchen_service',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+AUTH_USER_MODEL = "kitchen.Cook"
