b8573782548ad49c76919ecd9df2ea64c412d7da
Yurii <yura.gvozd.2002@gmail.com>
2026-08-26 13:03:08 +0300

    setup app routing and index view

---
+++ kitchen/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from kitchen import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app_name = 'kitchen'
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from kitchen.models import Cook, Ingredient, Dish, DishType
+
+
 # Create your views here.
+def index(request):
+    num_cooks = Cook.objects.all().count()
+    num_ingredients = Ingredient.objects.all().count()
+    num_dishes = Dish.objects.all().count()
+    num_dish_types = DishType.objects.all().count()
+
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    context = {
+        'num_cooks': num_cooks,
+        'num_ingredients': num_ingredients,
+        'num_dishes': num_dishes,
+        'num_dish_types': num_dish_types,
+        'num_visits': num_visits + 1,
+    }
+
+    return render(request, 'kitchen/index.html', context=context)
